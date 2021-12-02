6d0480117d72f55567ece25d373deb24f0a90e9a
Helisson Menezes <heli-menezes@hotmail.com>
2021-12-02 16:27:31 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,6 +9,42 @@ public class PrimeiroPrograma {
         System.out.println("Hello World! " + (a + b));*/
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livros livro1 = new Livros("As 46 leis do poder",  300);
+        System.out.println(livro1);
+    }
+}
+class Livros {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livros(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livros{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
 
